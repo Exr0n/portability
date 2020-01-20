7b1a7d49010b1de48af670f766e168819b70dee3
Exr0n <spotyie@gmail.com>
2020-01-19 20:20:48 -0800

    fix setup

--- setup.zsh
+++ setup.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 
-for n in `ls -a | grep -vE -e "^setup\.zsh$" -e "^\.{1,2}$" -e "^\.git" -e "^scripts\.zsh$"`; do
+for n in `ls -a | grep -vE -e "^setup\.zsh$" -e "^.git" -e "^\.{1,2}$" -e "^scripts\.zsh$"`; do
 	echo "Replacing $n..."
 	mv "/Users/$USER/$n" "/Users/$USER/$n $(date)"
 	cp -r "./$n" "/Users/$USER/$n"
