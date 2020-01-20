7aed8fc6eeb1820617af9e51255b31b6a44e9156
Exr0n <spotyie@gmail.com>
2020-01-20 08:26:50 -0800

    update setup.zsh interactivly asks if it should replace a file

--- setup.zsh
+++ setup.zsh
@@ -1,10 +1,21 @@
 #!/bin/zsh
 
 for n in `ls -a | grep -vE -e "^setup\.zsh$" -e "^.git" -e "^\.{1,2}$" -e "^scripts\.zsh$"`; do
-	echo "Replacing $n..."
-	mv "/Users/$USER/$n" "/Users/$USER/$n $(date)"
+	if [[ -e ~/$n ]]; then
+		color="\e[2;31m"
+		echo "$color" "File $n exists! Do you want to replace it? (y|n)"
+		read inp
+		if [[ "$inp" != "y" ]]; then
+			continue
+		fi
+		mv "/Users/$USER/$n" "/Users/$USER/$n $(date)"
+	fi
+
+	color="\e[2;32m"
+
+	echo "$color" "Installing $n..."
 	cp -r "./$n" "/Users/$USER/$n"
 done
 
-echo "Running setup scripts..."
+echo "\e[0mRunning setup scripts..."
 ./scripts.zsh
